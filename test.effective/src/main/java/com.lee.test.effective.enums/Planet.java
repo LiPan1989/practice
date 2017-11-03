4ef84f2ac153f64ab8db64451b36bb2287414730
LiPan1989 <512761742@qq.com>
2017-11-03 23:59:52 +0800

    efffective java enums

---
+++ test.effective/src/main/java/com.lee.test.effective.enums/Planet.java
@@ -0,0 +1,34 @@
+package com.lee.test.effective.enums;
+
+/**
+ * Created by LIPAN on 2017/11/3.
+ */
+public enum Planet {
+    MERCURY(3.202e+13, 2.439e6),
+    VENUS(4.689e+24, 6.052e6),
+    EARTH(5.975e+24, 6.378e6);
+    private final double mass;
+    private final double radius;
+    private final double surfaceGravity;
+
+    private static final double G = 6.67300E-11;
+
+    Planet(double mass, double radius) {
+        this.mass = mass;
+        this.radius = radius;
+        this.surfaceGravity = G + mass / (radius * radius);
+    }
+
+    public double mass(){
+        return this.mass;
+    }
+
+    public double radius() {
+        return this.radius;
+    }
+
+    public double surfaceGravity(){
+        return mass * surfaceGravity;
+    }
+
+}
---
+++ test.effective/src/main/java/com.lee.test.effective.enums/WeightTable.java
@@ -0,0 +1,14 @@
+package com.lee.test.effective.enums;
+
+/**
+ * Created by LIPAN on 2017/11/3.
+ */
+public class WeightTable {
+    public static void main(String[] args) {
+        double earthWeight = Double.parseDouble(args[0]);
+        double mass = earthWeight / Planet.EARTH.surfaceGravity();
+        for(Planet p : Planet.values()) {
+            System.out.printf("Weigth on %s is %f%n", p, p.surfaceGravity());
+        }
+    }
+}
