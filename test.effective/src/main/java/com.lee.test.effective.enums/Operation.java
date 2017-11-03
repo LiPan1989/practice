9b32aaed6cff958efeec9d13e939775072d9a123
LiPan1989 <512761742@qq.com>
2017-11-04 00:37:14 +0800

    efffective java enums

---
+++ test.effective/src/main/java/com.lee.test.effective.enums/Operation.java
@@ -0,0 +1,55 @@
+package com.lee.test.effective.enums;
+
+/**
+ * Created by LIPAN on 2017/11/4.
+ */
+public enum Operation {
+    PLUS("+"){
+        double apply(double x, double y) {
+            return x + y;
+        }
+    },
+    MINUS("-") {
+        double apply(double x, double y) {
+            return x - y;
+        }
+    },
+    TIMES("*") {
+        double apply(double x, double y) {
+            return x * y;
+        }
+    },
+    DIVIDE("/") {
+        double apply(double x, double y) {
+            return x / y;
+        }
+    };
+    private final String symbol;
+
+    Operation(String symbol) {
+        this.symbol = symbol;
+    }
+
+    abstract double apply(double x, double y);
+
+    public static void main(String[] args) {
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+        for (Operation op : Operation.values()) {
+
+            System.out.printf("%s: %f %s %f = %f%n", op, x, inverse(op), y, op.apply(x, y));
+
+        }
+    }
+
+    public static Operation inverse(Operation operation) {
+        switch (operation) {
+            case PLUS:return Operation.MINUS;
+            case MINUS:return Operation.PLUS;
+            case TIMES:return Operation.DIVIDE;
+            case DIVIDE:return Operation.TIMES;
+            default:
+                throw new AssertionError("Unknown operation:" + operation);
+        }
+    }
+}
---
+++ test.effective/src/main/java/com.lee.test.effective.enums/PayrollDay.java
@@ -0,0 +1,44 @@
+package com.lee.test.effective.enums;
+
+/**
+ * Created by LIPAN on 2017/11/4.
+ */
+public enum PayrollDay {
+    MONDAY(PayType.WEEKDAY),
+    TUESDAY(PayType.WEEKDAY),
+    WEDNESDAY(PayType.WEEKDAY),
+    THURSDAY(PayType.WEEKDAY),
+    FRIDAY(PayType.WEEKDAY),
+    STAURDAY(PayType.WEEKEND),
+    SUNDAY(PayType.WEEKEND);
+    private final PayType payType;
+
+    PayrollDay(PayType payType) {
+        this.payType = payType;
+    }
+
+    double pay(double hoursWorked, double payRate) {
+        return payType.pay(hoursWorked, payRate);
+    }
+
+    private enum PayType {
+        WEEKDAY {
+            double overtimePay(double hrs, double payRate) {
+                return hrs <= HOURS_PER_SHIFT ? 0 : (hrs - HOURS_PER_SHIFT) * payRate / 2;
+            }
+        },
+        WEEKEND {
+            double overtimePay(double hrs, double payRate) {
+                return hrs * payRate / 2;
+            }
+        };
+        private static final int HOURS_PER_SHIFT = 8;
+
+        abstract double overtimePay(double hrs, double payRate);
+
+        double pay(double hoursWorked, double payRate) {
+            double basePay = hoursWorked * payRate;
+            return basePay = overtimePay(hoursWorked, payRate);
+        }
+    }
+    }
