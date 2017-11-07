3c2ac61b4d83ce171b89b04c3204b64241d4d00c
LiPan1989 <512761742@qq.com>
2017-11-07 23:59:56 +0800

    efffective java enums

---
+++ test.effective/src/main/java/com/lee/test/effective/_36/Bigram.java
@@ -0,0 +1,35 @@
+package com.lee.test.effective._36;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by LIPAN on 2017/11/7.
+ */
+public class Bigram {
+    private final char first;
+    private final char second;
+
+    public Bigram(char first, char second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public boolean equals(Bigram bigram) {
+        return bigram.first == first && bigram.second == second;
+    }
+
+    public int hashCode() {
+        return 31 * first + second;
+    }
+
+    public static void main(String[] args) {
+        Set<Bigram> s = new HashSet<Bigram>();
+        for (int i = 0; i < 10; i++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                s.add(new Bigram(ch, ch));
+            }
+        }
+        System.out.println(s.size());
+    }
+}
