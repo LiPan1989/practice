a9a5d62debdceea8704cdb666375b155728efa97
LiPan1989 <512761742@qq.com>
2017-11-10 00:59:46 +0800

    efffective java generic

---
+++ test.effective/src/main/java/com/lee/test/effective/_28/Stack.java
@@ -0,0 +1,17 @@
+package com.lee.test.effective._28;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by LIPAN on 2017/11/10.
+ */
+public class Stack<E> {
+    public void pushAll(Iterator<? extends E> src) {
+
+    }
+
+    public void popAll(Collection<? super E> dst) {
+
+    }
+}
